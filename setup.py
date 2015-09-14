9d8709f16412027f2cb9d69730d7af0a8720c18a
TJ Torres <ttorres@stitchfix.com>
2015-09-14 10:25:46 -0700

    name changed and requirements.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fauxtograph',
-    version='0.0.1',
+    version='0.0.2',
     author='TJ Torres',
     author_email='ttorres@mit.edu',
     license='MIT',
