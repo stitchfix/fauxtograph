37a293f6f5c1cc01712aecc2b3322c6e9bff76ea
TJ Torres <ttorres@stitchfix.com>
2015-09-16 22:23:49 -0700

    Final edits for rollout.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fauxtograph',
-    version='0.1.0',
+    version='0.1.3',
     author='TJ Torres',
     author_email='ttorres@mit.edu',
     license='MIT',
