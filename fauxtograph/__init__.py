d24f9527087a343450e33079cdd01d6defa1a5d5
TJ Torres <ttorres@stitchfix.com>
2016-01-21 12:04:21 -0800

    Added additional public functions to importable list.

--- fauxtograph/__init__.py
+++ fauxtograph/__init__.py
@@ -1,5 +1,10 @@
 from . import fauxtograph
+from . import vaegan
 
 VAE = fauxtograph.VAE
 GAN = fauxtograph.GAN
 VAEGAN = fauxtograph.VAEGAN
+get_paths = fauxtograph.get_paths
+image_resize = fauxtograph.image_resize
+calc_fc_size = vaegan.calc_fc_size
+calc_im_size = vaegan.calc_im_size
