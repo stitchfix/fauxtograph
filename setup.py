5384ef3f32216a10254955e59e4576579603c82f
TJ Torres <ttorres@stitchfix.com>
2016-01-21 12:06:19 -0800

    Changed version number reflect slight fauxt generator tweak.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fauxtograph',
-    version='1.0.0',
+    version='1.0.1',
     author='TJ Torres',
     author_email='ttorres@mit.edu',
     license='MIT',
