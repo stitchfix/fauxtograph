393f402151126991dac1f2ee4cdd4c6aba817a5d
TJ Torres <ttorres@stitchfix.com>
2016-01-25 15:49:00 -0800

    Changed version number to reflect changes before uploading to PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fauxtograph',
-    version='1.0.2',
+    version='1.0.3',
     author='TJ Torres',
     author_email='ttorres@mit.edu',
     license='MIT',
